1b45b212374a8a22aa94a006f9be14f92c3c300a
Momen Khaled <momenk208@gmail.com>
2023-06-09 23:44:23 +0300

    added 2 charts for the gender with the foot length and one for the age and foot length

--- Momen_khaled.R
+++ Momen_khaled.R
@@ -1,61 +1,93 @@
+
+# import the required libraries
 library(ggplot2)
 
-# Importing the dataset
-# filepath <- file.path(getwd(), 'G4_howell.csv')
-df <- read.csv("./G2_anthropometry.csv")
+# Importing the data set
+data <- read.csv("./G2_anthropometry.csv")
 
 # Showing the first 5 values
-head(df)
+head(data)
 
 # Showing the last 5 values
-tail(df)
+tail(data)
 
 # Checking for the shape of the Values
-dim(df)
+dim(data)
 
-# Getting some stats
-str(df)
-summary(df)
+# Getting some stats about the data 
+str(data)
+summary(data)
 
 # Convert the Age column to integer
-df$age <- as.integer(df$age)
+data$age <- as.integer(data$age)
 
 # Convert the Gender Column
-df$gender <- ifelse(df$gender == 'cm', 'M', df$gender)
+data$gender <- ifelse(data$gender == 'cm', 'M', data$gender)
 
 # Impute missing values in the foot_length column
-mean_foot_length <- mean(df$foot_length, na.rm = TRUE)
-df$foot_length[is.na(df$foot_length)] <- mean_foot_length
-df$foot_length <- round(df$foot_length, 2)
+mean_foot_length <- mean(data$foot_length, na.rm = TRUE)
+data$foot_length[is.na(data$foot_length)] <- mean_foot_length
+data$foot_length <- round(data$foot_length, 2)
 
 # Remove 'cm' from the height column
-df$height <- gsub(' cm', '', df$height)
+data$height <- gsub(' cm', '', data$height)
 
 # Convert "height" column to numeric values
-df$height <- as.numeric(df$height)
+data$height <- as.numeric(data$height)
 
 # Create a pie chart of gender distribution
-gender_counts <- table(df$gender)
+gender_counts <- table(data$gender)
 pie(gender_counts, labels = names(gender_counts), autopct='%1.1f%%')
 title("Gender Distribution")
 
 # Create a scatter plot of height and age
-ggplot(df, aes(x = age, y = height)) +
+ggplot(data, aes(x = age, y = height)) +
   geom_point() +
   xlab("Age") +
   ylab("Height") +
   ggtitle("Relationship between Height and Age")
 
+# Create a scatter plot of age and foot_length
+ggplot(data, aes(x = age, y = foot_length)) +
+  geom_point() +
+  xlab("Age") +
+  ylab("Foot Length") +
+  ggtitle("Relationship between Age and Foot Length")
+
+# Create a scatter plot of height and foot_length
+ggplot(data, aes(x = height, y = foot_length)) +
+  geom_point() +
+  xlab("Height") +
+  ylab("Foot Length") +
+  ggtitle("Relationship between Height and Foot Length")
+
+# Create a scatter plot of age and foot_length
+ggplot(data, aes(x = age, y = foot_length)) +
+  geom_point() +
+  xlab("Age") +
+  ylab("Foot Length") +
+  ggtitle("Relationship between Age and Foot Length")
+
 # Find the highest height in males and females
-male_highest <- max(df$height[df$gender == 'M'])
-female_highest <- max(df$height[df$gender == 'F'])
+male_highest <- max(data$height[data$gender == 'M'])
+female_highest <- max(data$height[data$gender == 'F'])
 cat("Highest height in males:", male_highest, "\n")
 cat("Highest height in females:", female_highest, "\n")
 
 # Create a histogram of female heights
-female_height <- df$height[df$gender == 'F']
+female_height <- data$height[data$gender == 'F']
 hist(female_height, breaks = 10, xlab = "Height", ylab = "Frequency", main = "Histogram of Female Height")
 
 # Create a histogram of male heights
-male_height <- df$height[df$gender == 'M']
+male_height <- data$height[data$gender == 'M']
 hist(male_height, breaks = 10, xlab = "Height", ylab = "Frequency", main = "Histogram of Male Height")
+
+
+# Create a histogram of male foot_length
+male_foot_length <- data$foot_length[data$gender == 'M']
+hist(male_foot_length, breaks = 10, xlab = "Foot Length", ylab = "Frequency", main = "Histogram of Male Foot Length")
+
+
+# Create a histogram of female foot_length
+female_foot_length <- data$foot_length[data$gender == 'F']
+hist(female_foot_length, breaks = 10, xlab = "Foot Length",ylab = "Frequency", main = "Histogram of Male Foot Length")
